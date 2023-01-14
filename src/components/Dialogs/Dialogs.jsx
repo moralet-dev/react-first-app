48dae2a87a0633cf84ffa5fd1371db5779ee4cb1
Bohdan Kushnarenko <kushnarenko.dev@gmail.com>
2023-01-15 00:19:17 +0200

    Dialogs styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,20 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={`${s.dialog} ${s.active}`}>Jack</div>
+                <div className={s.dialog}>Bill</div>
+                <div className={s.dialog}>Ashley</div>
+                <div className={s.dialog}>Di</div>
+                <div className={s.dialog}>Valery</div>
+                <div className={s.dialog}>Andrew</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How are you?</div>
+
+            </div>
         </div>)
 }
 
