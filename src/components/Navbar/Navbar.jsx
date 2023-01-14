a26334a55eb63ed806668d858d83f37414192e86
Bohdan Kushnarenko <kushnarenko.dev@gmail.com>
2023-01-14 22:35:35 +0200

    Routers. React-router-dom

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className={s.nav}>
             <div className={s.item}><a href='/profile'>Profile</a></div>
-            <div className={s.item}><a href='/messages'>Messages</a></div>
+            <div className={s.item}><a href='/dialogs'>Dialogs</a></div>
             <div className={s.item}><a href='/news'>News</a></div>
             <div className={s.item}><a href='/music'>Music</a></div>
             <div className={s.item}><a href='/settings'>Settings</a></div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,9 +6,9 @@ function Profile() {
     return (
         <div>
             <div className={s.content_banner}>
-                <img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg'/>
+                <img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg' alt=''/>
             </div>
-            <div>
+            <div className={s.profile_photo}>
                 <img src='https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg'/>
             </div>
             <MyPosts/>
